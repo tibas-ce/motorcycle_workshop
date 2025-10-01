efb61a3ceb7c432bcf7b56d0b24bcd9c6d56e37a
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-10-01 06:37:04 -0300

    feat(auth): adiciona model User com Devise e campos customizados

---
+++ db/migrate/20251001092939_devise_create_users.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Campos customizados
+      t.string :name, null: false
+      t.string :phone_number
+      t.string :address
+      t.string :role, default: "cliente" # "cliente", "admin", "mecanico"
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :role
+  end
+end
