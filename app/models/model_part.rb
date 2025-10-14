bca18943c9ec060eb0b4754870dffe8664338591
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-10-14 16:34:54 -0300

    feat(ModelPart): adiciona ModelPart com migração inicial

---
+++ app/models/model_part.rb
@@ -0,0 +1,4 @@
+class ModelPart < ApplicationRecord
+  belongs_to :model_motorcycle
+  belongs_to :part
+end
---
+++ db/migrate/20251014193116_create_model_parts.rb
@@ -0,0 +1,14 @@
+class CreateModelParts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :model_parts do |t|
+      t.references :model_motorcycle, null: false, foreign_key: true
+      t.references :part, null: false, foreign_key: true
+      t.boolean :mandatory_review, default: false
+      t.integer :km_replacement
+
+      t.timestamps
+    end
+
+    add_index :modelo_pecas, [ :modelo_moto_id, :peca_id ], unique: true
+  end
+end
