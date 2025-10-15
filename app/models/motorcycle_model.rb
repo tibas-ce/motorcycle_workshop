0b25bcf7b954f9b327a0229f4440e1edd998126a
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-10-15 09:09:27 -0300

    feat(motorcycle_model): Implementa modelo para cadastro de Modelos de Motocicleta

--- app/models/motorcycle_model.rb
+++ app/models/motorcycle_model.rb
@@ -1,2 +1,8 @@
 class MotorcycleModel < ApplicationRecord
+  has_many :motorcycles
+  has_many :model_parts, dependent: :destroy
+  has_many :parts, through: :model_parts
+
+  validates :name, :displacement, presence: true
+  validates :warranty_months, :warranty_km, numericality: { greater_than: 0 }
 end
